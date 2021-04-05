af652506fbaf56d75846c5cf767c5bb60c4183ca
Michal Broniewicz <mbroniewicz@outlook.com>
2021-04-06 00:09:01 +0200

    ViewPager with three empty fragments

---
+++ app/src/main/java/io/github/thefrsh/stratus/fragment/NotificationsFragment.kt
@@ -0,0 +1,17 @@
+package io.github.thefrsh.stratus.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.thefrsh.stratus.R
+
+class NotificationsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+}
